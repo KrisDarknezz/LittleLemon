770e452b704dcbf58dc17db1c2d2bd1c488b16bb
Luis Crisanto <alfret_12_8@hotmail.com>
2023-04-06 22:32:58 -0600

    enable drf api

--- litlelemon/restaurant/urls.py
+++ litlelemon/restaurant/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path 
 from .views import *
 from . import views
+
   
 urlpatterns = [ 
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
