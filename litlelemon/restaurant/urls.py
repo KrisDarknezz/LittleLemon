011c050f19cf431fa62c121984f79da454debaf9
Luis Crisanto <alfret_12_8@hotmail.com>
2023-04-07 12:33:36 -0600

    enabling authentication for api using djoser

--- litlelemon/restaurant/urls.py
+++ litlelemon/restaurant/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
   
 urlpatterns = [ 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('menu/', views.MenuItemsView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- litlelemon/restaurant/views.py
+++ litlelemon/restaurant/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework import generics, viewsets
 from .models import Menu, Booking
 from .serializers import *
+from rest_framework.permissions import IsAuthenticated
 
 def index(request):
     return render(request, "index.html",{})
@@ -12,11 +13,14 @@ def index(request):
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
     
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
